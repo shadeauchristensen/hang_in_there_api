f2f65636afa51bc81f99c439619668c43409fb25
Jono Sommers <jonosommers95@gmail.com>
2024-12-19 14:47:31 -0600

    refactored the controller methods to work with the refactored serializer

--- app/controllers/api/v1/posters_controller.rb
+++ app/controllers/api/v1/posters_controller.rb
@@ -3,19 +3,23 @@ class Api::V1::PostersController < ApplicationController
         posters = Poster.all
         options = {}
         options[:meta] = {count: posters.count}
+
         if  params[:sort] == 'asc'
             posters = posters.order(:created_at)
         elsif
             params[:sort] == 'desc'
             posters = posters.order(created_at: :desc)
-        elsif 
-            params[:name] == "#{params[:name]}"
+        end
+
+        if params[:name] == "#{params[:name]}"
             posters = Poster.where("name ILIKE ?", "%#{params[:name]}%").order(:name)
-        elsif 
-            params[:min_price] == "#{params[:min_price]}"
+        end
+
+        if params[:min_price] == "#{params[:min_price]}"
             posters = posters.where("price >= ?", params[:min_price]).order(:price)
-        elsif 
-            params[:max_price] == "#{params[:max_price]}"
+        end
+
+        if params[:max_price] == "#{params[:max_price]}"
             posters = posters.where("price <= ?", params[:max_price]).order(:price)
         end
 
@@ -27,15 +31,20 @@ class Api::V1::PostersController < ApplicationController
     end
 
     def create
-        render json: Poster.create(poster_params)
+        poster = Poster.create(poster_params)
+        render json: PosterSerializer.new(poster)
     end
 
     def update
-        render json: Poster.update(params[:id], poster_params)
+        poster = Poster.find(params[:id])
+        poster.update(poster_params)
+        render json: PosterSerializer.new(poster)
     end
     
     def destroy
-        render json: Poster.delete(params[:id])
+        poster = Poster.find(params[:id])
+        poster.destroy
+        render json: PosterSerializer.new(poster)
     end
 
     private
